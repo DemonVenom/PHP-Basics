5c7c905dfd458c90eb3e50c1a6ab97962d2eb17c
Henry Hoffman <47870005+DemonVenom@users.noreply.github.com>
2022-01-11 16:35:31 -0500

    init commit

---
+++ php-basics-s1v3/inc/hello.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is our first "Hello World!" script
+ *
+ * @author: Henry Hoffman
+ *
+ */
+
+// our first php script
+echo 'Hello World!'; // done
+// echo 'Hello Treehouse!'; // done
+// you can make a single line comment as long as you want...
+// this is a second comment line
+
+/*
+ line 1
+ line 2
+ line 3
+ */
+
+?>
